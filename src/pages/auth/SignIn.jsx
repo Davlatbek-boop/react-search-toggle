41535f529c401d6429ca798fec320ae8e93cba38
Davlatbek <salimovdavlat0526@.gmail.com>
2025-07-02 18:51:21 +0500

    logs uchun menu qo'shildi

--- src/pages/auth/SignIn.jsx
+++ src/pages/auth/SignIn.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React from 'react';
+import { Button, Checkbox, Form, Input } from 'antd';
 
-const SignIn = () => {
-  return (
-    <div><h1>Signin</h1></div>
-  )
-}
+const onFinish = (values) => {
+  console.log('Success:', values);
+};
 
-export default SignIn
+const onFinishFailed = (errorInfo) => {
+  console.log('Failed:', errorInfo);
+};
+
+const App = () => (
+  <div
+    style={{
+      height: '100vh',              // Butun ekran balandligi
+      display: 'flex',
+      justifyContent: 'center',     // Gorizontal markaz
+      alignItems: 'center',         // Vertikal markaz
+      background: '#f5f5f5',        // Orqa fon (ixtiyoriy)
+    }}
+  >
+    <Form
+      name="basic"
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 16 }}
+      style={{ maxWidth: 600, width: '100%' }}
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[{ required: true, message: 'Please input your username!' }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: 'Please input your password!' }]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item name="remember" valuePropName="checked" label={null}>
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+
+      <Form.Item label={null}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  </div>
+);
+
+export default App;
--- src/pages/auth/SignUp.jsx
+++ src/pages/auth/SignUp.jsx
@@ -1,9 +1,95 @@
-import React from 'react'
+import React from 'react';
+import { Button, Form, Input } from 'antd';
 
 const SignUp = () => {
-  return (
-    <div><h1>SignUp</h1></div>
-  )
-}
+  const [form] = Form.useForm();
 
-export default SignUp
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  // Parol tasdiqlash validator
+  const validateConfirmPassword = (_, value) => {
+    if (!value || form.getFieldValue('password') === value) {
+      return Promise.resolve();
+    }
+    return Promise.reject(new Error('Passwords do not match!'));
+  };
+
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        background: '#f0f2f5',
+      }}
+    >
+      <Form
+        form={form}
+        name="signup"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        style={{ maxWidth: 600, width: '100%' }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[{ required: true, message: 'Please input your username!' }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[
+            { required: true, message: 'Please input your email!' },
+            { type: 'email', message: 'Please enter a valid email!' },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: 'Please input your password!' }]}
+          hasFeedback
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          label="Confirm"
+          name="confirm"
+          dependencies={['password']}
+          hasFeedback
+          rules={[
+            { required: true, message: 'Please confirm your password!' },
+            { validator: validateConfirmPassword },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Sign Up
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+
+export default SignUp;
